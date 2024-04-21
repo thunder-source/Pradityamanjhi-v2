4b4120866c403f109821dbbc01df9546745531bb
thunder-source <majhipraditya@gmail.com>
2024-04-22 00:07:13 +0530

    update live links

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -52,7 +52,7 @@ export const PROJECTS_CARD: ProjectCardProps[] = [
     description:
       "Visualize JSON data format in form of a tree or graph. Application is made using Nextjs, Tailwindcss and Monaco Editor. Design image can be downloaded from this app.",
     // sourceCodeHref: "https://github.com/BUMBAIYA/jsontree",
-    liveWebsiteHref: "https://jsontree.vercel.app",
+    liveWebsiteHref: "https://form-builder-client-rosy.vercel.app/",
   },
   {
     name: "Crypto Tracker",
@@ -63,7 +63,7 @@ export const PROJECTS_CARD: ProjectCardProps[] = [
     description:
       "Keep track of projects and tasks in different categories. Drag and drop card from tables. Assign user and priority to task and many more features. This app is made using Reactjs, Typescript, Tailwindcss and React-beautiful-dnd library.",
     // sourceCodeHref: "https://github.com/BUMBAIYA/kanban",
-    liveWebsiteHref: "https://kannban-board.vercel.app",
+    liveWebsiteHref: "https://crypto-pro-drab.vercel.app/",
   },
   {
     name: "Tesla clone",
@@ -74,7 +74,7 @@ export const PROJECTS_CARD: ProjectCardProps[] = [
     description:
       "Many short and fun games using Reactjs, Typescript and Tailwindcss with collaboration with other developers",
     // sourceCodeHref: "https://github.com/BUMBAIYA/ManyGames",
-    liveWebsiteHref: "https://manygames.vercel.app",
+    liveWebsiteHref: "https://tesla-reactjs.vercel.app/",
   },
   {
     name: "My portfolio",
--- src/data/siteMetaData.mjs
+++ src/data/siteMetaData.mjs
@@ -6,7 +6,7 @@ export const siteMetadata = {
   language: "en-us",
   theme: "light",
   siteName: "pradityamanjhi.me",
-  siteUrl: "https://pradityamanjhi.me",
+  siteUrl: "https://pradityamanjhi-v2.vercel.app/",
   siteRepo: "https://github.com/thunder-source/Pradityamanjhi-v2",
   siteLogo: "/favicon.ico",
   image: "/static/homepage.png",
