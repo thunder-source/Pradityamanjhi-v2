8039fad9c959fe538b856ad5452dc5c491263f18
thunder-source <majhipraditya@gmail.com>
2024-04-24 10:58:14 +0530

    added Analytics , SpeedInsights

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Document() {
   return (
@@ -7,6 +9,8 @@ export default function Document() {
       <body className="bg-background text-zinc-950 antialiased selection:bg-accent selection:text-background">
         <Main />
         <NextScript />
+        <SpeedInsights />
+        <Analytics />
       </body>
     </Html>
   );
