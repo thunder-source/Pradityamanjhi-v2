d5902a49e423b6186f6d357708c0753e503fc764
thunder-source <majhipraditya@gmail.com>
2024-05-06 09:30:13 +0530

    project update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const { fontFamily } = require("tailwindcss/defaultTheme")
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 module.exports = {
   darkMode: "class",
